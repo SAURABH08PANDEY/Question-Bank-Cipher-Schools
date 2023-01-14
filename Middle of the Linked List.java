0e544b77d2542bbc0e949f2f7943d90fbc16556b
SAURABH08PANDEY <80577220+SAURABH08PANDEY@users.noreply.github.com>
2023-01-15 01:16:56 +0530

    Update Middle of the Linked List.java
    time: O(n)
    space: O(1)

--- Middle of the Linked List.java
+++ Middle of the Linked List.java
@@ -4,10 +4,10 @@
 
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode fast=new ListNode(0);
-        ListNode slow=new ListNode(0);
-        fast.next=head;
-        slow.next=head;
+        //ListNode fast=new ListNode(0);
+        //ListNode slow=new ListNode(0);
+        ListNode fast.next=head;
+        ListNode slow.next=head;
 
         while(fast.next!=null&&fast.next.next!=null){
             fast=fast.next.next;
